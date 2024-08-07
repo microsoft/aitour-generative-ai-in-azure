3fa10108edf6e1fbd1385329e1675c60d922fbc3
Henk  Boelman <henk@boelman.eu>
2024-08-07 13:44:54 +0000

    setup environment

--- src/setup.sh
+++ src/setup.sh
@@ -1,27 +1,34 @@
 #!/bin/bash  
 
-prefix="aitour"
+prefix="BRK440"
 
 ai_resource_name="$prefix-$(shuf -i 1000-9999 -n 1)"
 
-#ai_resource_name="aitour-9020"
+#ai_resource_name="BRK440"
+location="swedencentral"
 
 echo "Resource name: $ai_resource_name"  
 
 # Create the resource group
 ai_resource_name_resource_group_name=$ai_resource_name"_RG"
 echo "Creating resource group: $ai_resource_name_resource_group_name"
-az group create --name $ai_resource_name_resource_group_name --location eastus > null
+az group create --name $ai_resource_name_resource_group_name --location $location > null
 
 # Create the Hub
 ai_resource_name_hub_name=$ai_resource_name"-hub"
 echo "Creating AI Studio Hub: $ai_resource_name_hub_name"
 az ml workspace create --kind hub --resource-group $ai_resource_name_resource_group_name --name $ai_resource_name_hub_name > null
 
+# Create project in Hub
+hub_id=$(az ml workspace show  -g $ai_resource_name_resource_group_name --name $ai_resource_name_hub_name --query id --output tsv)
+ai_resource_project_name=$ai_resource_name"-project"
+echo "Creating AI Studio Project: $ai_resource_project_name"
+az ml workspace create --kind project --resource-group $ai_resource_name_resource_group_name --name $ai_resource_project_name --hub-id $hub_id > null
+
 # Create a Azure AI Service Account
 ai_resource_ai_service=$ai_resource_name"-aiservice"
 echo "Creating AI Service Account: $ai_resource_ai_service"
-az cognitiveservices account create --kind AIServices --location eastus --name $ai_resource_ai_service --resource-group $ai_resource_name_resource_group_name --sku S0 --yes > null
+az cognitiveservices account create --kind AIServices --location $location --name $ai_resource_ai_service --resource-group $ai_resource_name_resource_group_name --sku S0 --yes > null
 
 # Deploying GPT-4o in Azure AI Service
 echo "Deploying GPT-4o"
@@ -30,11 +37,13 @@ az cognitiveservices account deployment create --name $ai_resource_ai_service --
 # Adding connection to the AI Hub
 echo "Adding AI Service Connection to the HUB"
 ai_service_resource_id=$(az cognitiveservices account show --name $ai_resource_ai_service --resource-group $ai_resource_name_resource_group_name --query id --output tsv)
+ai_service_api_key=$(az cognitiveservices account keys list --name $ai_resource_ai_service --resource-group $ai_resource_name_resource_group_name --query key1 --output tsv)
 
 rm connection.yml   
-echo "name: $ai_resource_name_hub_name" >> connection.yml  
+echo "name: $ai_resource_ai_service" >> connection.yml  
 echo "type: azure_ai_services" >> connection.yml  
 echo "endpoint: https://eastus.api.cognitive.microsoft.com/" >> connection.yml  
+echo "api_key: $ai_service_api_key" >> connection.yml  
 echo "ai_services_resource_id:  $ai_service_resource_id" >> connection.yml  
 
 az ml connection create --file connection.yml --resource-group $ai_resource_name_resource_group_name --workspace-name  $ai_resource_name_hub_name > null
@@ -43,4 +52,21 @@ az ml connection create --file connection.yml --resource-group $ai_resource_name
 echo "Disable storage SAS keys"
 storage_resource_id=$(az ml workspace show --name $ai_resource_name_hub_name --resource-group $ai_resource_name_resource_group_name --query storage_account --output tsv)
 storage_name=$(echo $storage_resource_id | awk -F'/' '{print $NF}') 
-az storage account update --name $storage_name --resource-group $ai_resource_name_resource_group_name --allow-shared-key-access false   > null
+#az storage account update --name $storage_name --resource-group $ai_resource_name_resource_group_name --allow-shared-key-access false   > null
+az storage account update --name $storage_name --resource-group $ai_resource_name_resource_group_name --min-tls-version TLS1_2  
+
+# AI Search
+tmp_name=$ai_resource_name"-aisearch"
+ai_resource_ai_search="${tmp_name,,}"
+az search service create --name $ai_resource_ai_search --resource-group $ai_resource_name_resource_group_name --sku Free --partition-count 1 --replica-count 1
+
+search_url="https://"$ai_resource_ai_search".search.windows.net"
+search_key=$(az search admin-key show --service-name $ai_resource_ai_search --resource-group $ai_resource_name_resource_group_name --query primaryKey --output tsv)
+
+rm connection_search.yml   
+echo "name: $ai_resource_ai_search" >> connection_search.yml  
+echo "type: azure_ai_search" >> connection_search.yml  
+echo "endpoint: $search_url" >> connection_search.yml  
+echo "api_key: $search_key" >> connection_search.yml  
+
+az ml connection create --file connection_search.yml --resource-group $ai_resource_name_resource_group_name --workspace-name  $ai_resource_name_hub_name > null
